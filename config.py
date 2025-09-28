65ab6d45124bc6774341be91bc3338856ededfee
mtiessler <maxtiessler4@gmail.com>
2025-09-28 09:39:30 +0200

    added embeddings and remote neo4j

--- config.py
+++ config.py
@@ -6,14 +6,18 @@ class Config:
     DEBUG = os.environ.get('FLASK_DEBUG', 'False').lower() == 'true'
 
     # Neo4j Configuration
-    NEO4J_URI = os.environ.get('NEO4J_URI', 'bolt://localhost:7687')
+    NEO4J_URI = os.environ.get('NEO4J_URI', 'bolt://10.4.63.10:7687')
     NEO4J_USER = os.environ.get('NEO4J_USER', 'neo4j')
     NEO4J_PASSWORD = os.environ.get('NEO4J_PASSWORD', '12345678')
     NEO4J_DATABASE = os.environ.get('NEO4J_DATABASE', 'neo4j')
 
     # Models Configuration
     TFREX_MODEL = 'quim-motger/t-frex-bert-base-uncased'
-    EMBEDDING_MODEL = 'all-MiniLM-L6-v2'
+    EMBEDDING_MODELS = {
+        'allmini': 'all-MiniLM-L6-v2',
+        'sentence-t5': 'sentence-transformers/sentence-t5-base'
+    }
+    DEFAULT_EMBEDDING_MODEL = 'allmini'
     OLLAMA_MODEL = os.environ.get('OLLAMA_MODEL', 'qwen:1.8b')
     OLLAMA_BASE_URL = os.environ.get('OLLAMA_BASE_URL', 'http://localhost:11434')
 
